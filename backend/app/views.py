5272fdec399abbb2139781bc1b7ab854f3bfc30b
HuyNguyenUTA <huy.nguyen.uta@gmail.com>
2022-11-13 22:56:10 -0600

    q

--- backend/app/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.views.generic import ListView
-from .models import Student
-
-# Create your views here.
-class StudentListView(ListView):
-    model = Student
